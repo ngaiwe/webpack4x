ee48fa09c0f5ff71eaf667aa962aefa49c28d84b
weiran <weiran@vipkid.com.cn>
2019-04-02 20:43:45 +0800

    修改配置

--- config/index.js
+++ config/index.js
@@ -4,11 +4,11 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/', // 设置启动后访问路径前缀
+    assetsPublicPath: '', // 设置启动后访问路径前缀
     proxyTable: {}, // 代理
     // devserver 所需配置
     host: '', // ip 如果需要设置host 0.0.0.0
-    port: 8081, // 端口
+    port: 8080, // 端口
     notifyOnErrors: true, // 是否显示消息提示信息
     // Source Maps
     devtool: 'cheap-module-eval-source-map',
@@ -19,7 +19,7 @@ module.exports = {
     // 打包输出path 
     assetsRoot: path.resolve(__dirname, '../dist'), // 打包后的地址
     assetsSubDirectory: 'static', // 静态目录拷贝出的存放地址
-    assetsPublicPath: '/', // 打包后的入口地址 html引入srcipt的src地址
+    assetsPublicPath: '', // 打包后的入口地址 html引入srcipt的src地址
     // 是否启用source map source类型
     productionSourceMap: true,
     devtool: '#source-map',
@@ -34,7 +34,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     productionSourceMap: true,
     devtool: '#source-map',
     productionGzip: true,
